33bdbcd797b67f4a8e50dc0095fe02f636a910fa
aykhanstoic <nazaralievajhan@gmail.com>
2026-09-11 00:57:56 +0400

    Add createTapPad, one JS handle for taps and purchases

--- sdk/tappad-js/index.js
+++ sdk/tappad-js/index.js
@@ -3,3 +3,4 @@ export { parseUid } from "./src/uid.js";
 export { TapPadError } from "./src/error.js";
 export { createServerClient, isFinal, isSuccess } from "./src/server.js";
 export { connectBridge, parsePadEvent } from "./src/bridge.js";
+export { createTapPad, DEFAULT_BRIDGE_URL, DEFAULT_SERVER_URL } from "./src/tappad.js";
